0da88379c0a50177ec9df25ba7a663464db84654
Justice Nyame <94219365+Jnyame21@users.noreply.github.com>
2024-07-07 12:10:46 +0000

    version 0.02

--- setup.py
+++ setup.py
@@ -2,8 +2,8 @@ from setuptools import setup, find_packages
 
 setup(
     name='bolcan',
-    version='0.0.1',
-    packages=find_packages('tests*', 'docs*'),
+    version='0.0.3',
+    packages=find_packages(),
     install_requires=[
         'pandas',
     ],
